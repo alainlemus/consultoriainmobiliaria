65d65dcd64ffeaacc9f8239cc6b8b02036c4f237
alainlemus <alainttlm@gmail.com>
2026-06-02 15:45:24 -0600

    feat: version 1.2.8

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'version' => env('APP_VERSION', '1.2.7'),
+    'version' => env('APP_VERSION', '1.2.8'),
 
     /*
     |--------------------------------------------------------------------------
